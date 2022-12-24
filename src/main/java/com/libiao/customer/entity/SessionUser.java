5a7d619e66667c5e9e220507a7e95849089caafd
yangjun <yangjun@jlishop.com>
2022-12-24 13:30:31 +0800

    添加session

--- src/main/java/com/libiao/customer/entity/SessionUser.java
+++ src/main/java/com/libiao/customer/entity/SessionUser.java
@@ -1,4 +1,25 @@
 package com.libiao.customer.entity;
 
-public class SessionUser {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class SessionUser implements Serializable {
+
+    private long id;
+
+    private String username;
+
+    private String permission;
+
+    private String nickname;
+
+    private String phoneNum;
+
+    private String email;
+
+    private String status;
+
+
 }
